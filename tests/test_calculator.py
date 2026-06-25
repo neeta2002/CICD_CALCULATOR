20e2f05350838c94a3b6dec8dbb71887617ee611
Django project <nitadhangar2002@gmail.com>
2026-06-25 14:11:15 +0530

    Initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,14 @@
+from app.calculator import add,subtract,multiply,divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert add(5, 2) == 3
+
+def test_multiply():
+    assert add(2, 4) == 8
+
+def test_divide():
+    assert add(10, 2) == 5
